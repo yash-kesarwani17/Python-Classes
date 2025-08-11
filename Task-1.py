0d900f15383dbb04430727066221c76e075d7a5b
yash-kesarwani17 <officialid.yashkesarwani@gmail.com>
2025-08-11 12:51:22 +0530

    Basic Python Concepts

---
+++ Task-1.py
@@ -0,0 +1,10 @@
+first_num=int(input("Enter first number: "))
+second_num=int(input("Enter second number: "))
+sum= first_num+second_num
+difference= first_num-second_num
+multiply= first_num*second_num
+divison= first_num/second_num
+print("Addition: ",sum)
+print("Subtraction: ",difference)
+print("Multiplication: ",multiply)
+print("Divison: ",divison)
