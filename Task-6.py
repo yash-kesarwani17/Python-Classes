ae616a79681f652a6be546bb19268b1b055dd07f
yash-kesarwani17 <officialid.yashkesarwani@gmail.com>
2025-09-06 15:51:02 +0530

    Functions & Modules in Python

---
+++ Task-6.py
@@ -0,0 +1,8 @@
+from math import *
+num=int(input("Enter Number: "))
+sq_root= sqrt((num))
+print("The square root of",num,"is",sq_root)
+logarithm= log(num)
+print("The logarithm of",num,"is",logarithm)
+value=sin(num)
+print("The sine of",num,"is",value)
